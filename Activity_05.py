b38f4355b2ca2adc963ef00f88264b536b7445ba
rajat6219 <89654096+rajat6219@users.noreply.github.com>
2021-08-28 17:24:11 +0530

    Update Activity_05.py

--- Activity_05.py
+++ Activity_05.py
@@ -1,7 +1,6 @@
-input_string = input('Enter 5 elements of a list separated by space ')
-user_list = input_string.split()
+input_string = input('Enter 5 elements of a list separated by comma ')
+user_list = input_string.split(',')
 print('list: ', user_list)
 for i in range(len(user_list)):
     user_list[i] = int(user_list[i])
 print("Sum = ", sum(user_list))
-
