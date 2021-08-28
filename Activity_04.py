82794b76363372e1e807d511f6efec0c0dbc4253
rajat6219 <89654096+rajat6219@users.noreply.github.com>
2021-08-28 14:28:37 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,4 +1,4 @@
 a=input("Type a number: ")
 b=input("Type another number: ")
 sum = int(a) + int(b)
-print("The sum of {0} and {1} is {2}" .format(a, b, sum))
+print(f"The sum of {a} and {b} is {sum}")
