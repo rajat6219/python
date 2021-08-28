e6455e0459bd69b0d78cbefbab18fac4f160b43b
rajat6219 <89654096+rajat6219@users.noreply.github.com>
2021-08-28 21:03:05 +0530

    Update Activity_06.py

--- Activity_06.py
+++ Activity_06.py
@@ -1,15 +1,18 @@
-input_string = input('Enter 5 elements of a list separated by space ')
+input_string = input("Enter 5 elements of a list separated by space ")
 user_list = input_string.split()
 
 sliced_list = user_list[0:3]
-print('sliced list: ', sliced_list)
+print("sliced list: ", sliced_list)
+
+reversed_list = user_list[::-1]
+print("reversed list: ", reversed_list)
 
 rep_list_1 = user_list
 rep_list_1[0] = 0
 rep_list_1[-1] = 0
-print("replaced list-1", rep_list_1)
+print("replaced list-1: ", rep_list_1)
 
 rep_list_2 = sliced_list
 rep_list_2[0] = 0
 rep_list_2[2] = 0
-print("replaced list-2", rep_list_2)
+print("replaced list-2: ", rep_list_2)
