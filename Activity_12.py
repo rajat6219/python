71961a3116d2211977936f828477f1bb94c88938
rajat6219 <89654096+rajat6219@users.noreply.github.com>
2021-08-29 19:23:26 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -1,4 +1,23 @@
-input_string = input("Enter 3 numbers separated by space ")
-list = input_string.split()
+def input_number():
+    input_string = input('Enter 3 numbers separated by space ')
+    list = input_string.split()
+    return list[0],list[1],list[2]
 
-print('{0} is the greatest number among {1},{2} and {3}'.format(max(list),list[0],list[1],list[2]))    
+def compare(a, b, c):
+    if (a >= b) and (a >= c):
+        return a
+    elif(b >= c):
+        return b 
+    else:
+        return c
+        
+def display(a, b, c, greatest):
+    print(f'{greatest} is the greatest number among {a},{b} and {c}')
+
+
+def main():
+    a,b,c = input_number()
+    greatest = compare(a,b,c)
+    display(a,b,c,greatest)
+main()
+
