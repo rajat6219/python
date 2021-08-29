2826a0d2e0319f8f8587d837cad22174bf3eb100
rajat6219 <89654096+rajat6219@users.noreply.github.com>
2021-08-29 21:59:16 +0530

    Update Activity_13.py

--- Activity_13.py
+++ Activity_13.py
@@ -1,15 +1,22 @@
+print('To find all the odd numbers between a start number till the stop number,')
 def input_number():
     start = int(input("Enter the start number: "))
     stop = int(input("Enter the stop number: "))
     return start,stop
   
 def iteration(start,stop):
+    list = []
     for num in range(start, stop + 1):
-        if num % 2 != 0:
-            print(num, end=",")
+        if (num%2 != 0):
+            list.append(num)
+    return list        
+
+def display(odd_numbers):
+    print(f'Required odd numbers are: {odd_numbers}')
     
 def main(): 
     start,stop = input_number()
-    iteration(start,stop)    
+    odd_numbers = iteration(start,stop)
+    display(odd_numbers)
     
 main()        
