a73c579b36a50565ab43312b0df785246186e5f7
rajat6219 <89654096+rajat6219@users.noreply.github.com>
2021-08-29 15:11:36 +0530

    Update Activity_09.py

--- Activity_09.py
+++ Activity_09.py
@@ -8,5 +8,7 @@ k=(l**2)+(b**2)+(h**2)
 volume_of_tromboloid = ((b**2)*(h**2))/(k**(1/2))
 print(f"Volume of tromboloid is {volume_of_tromboloid:.3f}")
 
-radius_of_sphere = (((3*volume_of_tromboloid)/(4*3.14159))**(1/3))
+import math
+
+radius_of_sphere = (((3*volume_of_tromboloid)/(4*math.pi))**(1/3))
 print(f"Radius of sphere is {radius_of_sphere:.3f}")
